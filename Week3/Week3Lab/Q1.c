4d3c44d7fa65bb4d1614c4d2dac347faf09995e9
petedouglas101 <c18762431@mytudublin.ie>
2021-03-16 18:49:03 +0000

    Allowed user to inout student for whose results they wish to view

--- Week3/Week3Lab/Q1.c
+++ Week3/Week3Lab/Q1.c
@@ -38,7 +38,7 @@ void main()
 		for (int j = 0; j < EXAMS; j++) {
 			total = total + exams[i][j];
 		}
-
+		
 		averages[i] = total / 3;
 
 	}
@@ -47,6 +47,22 @@ void main()
 	for (int i = 0; i < STUDENTS; i++)
 	{
 		printf("The average results for student %d are %d%%\n",(i + 1), averages[i] );
+	}
+
+	//ask user which of the students results they wish to see
+	int option = 0;
+	printf("Which of the students results do you wish to see? (0 - 3)\n");
+	scanf("%d", &option);
+
+	//search for the student the user chose, and display results
+	for (int i = 0; i < STUDENTS; i++) {
+		if (i == option) {
+			printf("You are seeing results for student %d\n", option);
+			for (int j = 0; j < EXAMS; j++) {
+				printf("%d%%", exams[i][j]);
+			}
+			
+		}
 	}
 	
 
